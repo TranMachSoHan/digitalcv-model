9696f0554378c1a2b9f239b2d9bdcc4f61359199
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-11 16:54:39 +0700

    make it better

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask,jsonify,request
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+from flask_cors import CORS
+import numpy as np
+import RecommenderModel 
+
+app = Flask(__name__)
+
+# 
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'Hello, from Flask!'
+
+@app.route("/recommend", methods = ['GET'])
+def recommend():
+    req_data = request.get_json()
+    courseList = req_data['courseList']
+    print(courseList)
+    r = RecommenderModel.rcmd(courseList)
+    print(r)
+    return jsonify(r)
+      
+if __name__ == '__main__':
+    app.run(debug=True, port=81)
