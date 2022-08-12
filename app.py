1540f4d333bd979c967b24eab8b443239ef1b3e1
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-12 12:03:13 +0700

    make it better

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # 
 CORS(app)
 
-@app.route('/')
+@app.route('/',methods = ['GET'])
 def index():
     return 'Hello, from Flask!'
 
@@ -24,4 +24,4 @@ def recommend():
       
 
 if __name__ == '__main__':
-    app.run(debug=True, port=33507)
+    app.run()
