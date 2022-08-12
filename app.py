11582a353dbe694fc1cc103edd0d908ae7644a9e
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-12 18:08:56 +0700

    model deployment success

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from copyreg import pickle
 from distutils.log import debug
 from flask import Flask,jsonify,request
 import RecommenderModel 
+import joblib
+import pandas
 
 app = Flask(__name__)
 
@@ -8,6 +11,14 @@ app = Flask(__name__)
 @app.route('/',methods = ['GET'])
 def index():
     return 'Hello, from Flask!'
+
+@app.route('/load_data', methods = ['GET'])
+def load_data():
+    try:
+        lookup_table = pickle.load('pickle_folder/rules.pkl')
+    except Exception as e:
+        return f"Exception {e}"
+    return f"my_pickle : {lookup_table}"
 
 @app.route("/recommend", methods = ['GET'])
 def recommend():
