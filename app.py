4a2cb484f722057da6b7cee3d9f71f608da6d39b
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-12 17:32:42 +0700

    model deployment success

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from distutils.log import debug
 from flask import Flask,jsonify,request
 import RecommenderModel 
-import joblib
-import pandas
 
 app = Flask(__name__)
 
@@ -11,22 +9,19 @@ app = Flask(__name__)
 def index():
     return 'Hello, from Flask!'
 
-@app.route('/load_data', methods = ['GET'])
-def load_data():
-    try:
-        lookup_table = joblib.load('pickle_folder/rules.pkl')
-    except Exception as e:
-        return f"Exception {e}"
-    return f"my_pickle : {lookup_table}"
-
 @app.route("/recommend", methods = ['GET'])
 def recommend():
+    # get request param json 
     req_data = request.get_json()
     courseList = req_data['courseList']
+
+    # avoid joblib cannot read pickle file 
     try:
         r = RecommenderModel.rcmd(courseList)
     except Exception as e:
         return f"Exception {e}"
+
+    # return json type 
     return jsonify(r)
       
 
