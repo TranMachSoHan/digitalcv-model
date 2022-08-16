c8034681ec30d48c13ad43276e0aa8c3fddaea8c
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-16 15:48:11 +0700

    RECOMMEND method to post

--- app.py
+++ app.py
@@ -1,10 +1,70 @@
 import pickle
 from flask import Flask,jsonify,request
-import RecommenderModel 
 import pandas
+import pickle
+NUM_RESULT = 5
 
 app = Flask(__name__)
 
+def load_data():
+    print("Initializing recommender model")
+
+    # loading rule pickle file 
+    try:
+        with open('pickle_folder/rules.pkl', 'rb') as f:
+            lookup_table = pickle.load(f) 
+    except Exception as e:
+        # if it's wrong then return None 
+        return None
+
+    print(lookup_table)
+    return lookup_table
+
+### convert the series consequents and list out the courses 
+def to_list_recommend_course(series,unique_course_list):
+    i = 0
+    for item in series:
+        for course in list(item):
+            # avoid duplicate course 
+            if course not in unique_course_list:
+                unique_course_list.append(course)
+
+    return unique_course_list
+
+# defining a function that recommends 10 most similar movies
+def rcmd(courses):
+    print('Fetching recommendations')
+    lookup_table = load_data()
+
+    # return empty course if there is exception 
+    if(lookup_table is None):
+        return {'courseName': []}
+
+    # initialize the df 
+    temp_df = lookup_table
+    resul_df = lookup_table
+
+    # save the course list as result with origin request param 
+    unique_course_list = []
+
+    for courseName in courses: 
+        # to avoid the duplicate course name in the url param 
+        if courseName not in unique_course_list:
+            unique_course_list.append(courseName)
+
+            # sort the lookup table by the factorized item
+            # continue use the previous df ==> combine more courses together  
+            temp_df = resul_df[resul_df["antecedents"].apply(lambda x: courseName in str(x))].sort_values(ascending=False,by='lift')
+            
+            # to avoid the temp df 
+            if len(temp_df) < NUM_RESULT:
+                print(courseName)
+                continue
+            resul_df = temp_df 
+
+    json = {}
+    json['course_list'] = to_list_recommend_course(resul_df['consequents'],unique_course_list)    
+    return json
 
 @app.route('/',methods = ['GET'])
 def index():
@@ -19,7 +79,7 @@ def load_data():
         return f"Exception {e}"
     return f"my_pickle : {lookup_table}"
 
-@app.route("/recommend", methods = ['GET'])
+@app.route("/recommend", methods = ['POST'])
 def recommend():
     # get request param json 
     req_data = request.get_json()
@@ -27,7 +87,7 @@ def recommend():
 
     # avoid joblib cannot read pickle file 
     try:
-        r = RecommenderModel.rcmd(courseList)
+        r = rcmd(courseList)
     except Exception as e:
         return f"Exception {e}"
 
