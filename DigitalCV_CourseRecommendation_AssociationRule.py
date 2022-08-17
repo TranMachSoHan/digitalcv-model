97ab6c046e650c71cc0918c1ccca35e704d7b856
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-08-18 00:24:42 +0700

    ADD timestamp to file csv

--- DigitalCV_CourseRecommendation_AssociationRule.py
+++ DigitalCV_CourseRecommendation_AssociationRule.py
@@ -9,13 +9,37 @@ from mlxtend.frequent_patterns import fpgrowth
 from mlxtend.frequent_patterns import association_rules
 
 from datetime import datetime
-except Exception, exc:
-    sttime = datetime.now().strftime('%Y%m%d_%H:%M:%S - ')
-    error = "ERROR! {}".format(exc)
-    log = 'log.txt'
-    with open(log, 'a') as logfile:
-        logfile.write(sttime + error + '\n')
-    raise
+
+import csv
+from datetime import datetime
+
+# Here we are storing our data in a
+# variable. We'll add this data in
+# our csv file
+rows = [['GeeksforGeeks1', 'GeeksforGeeks2'],
+        ['GeeksforGeeks3', 'GeeksforGeeks4'],
+        ['GeeksforGeeks5', 'GeeksforGeeks6']]
+  
+# Opening the CSV file in read and
+# write mode using the open() module
+with open(r'YOUR_CSV_FILE.csv', 'r+', newline='') as file:
+  
+    # creating the csv writer
+    file_write = csv.writer(file)
+  
+    # storing current date and time
+    current_date_time = datetime.now()
+  
+    # Iterating over all the data in the rows 
+    # variable
+    for val in rows:
+          
+        # Inserting the date and time at 0th 
+        # index
+        val.insert(0, current_date_time)
+          
+        # writing the data in csv file
+        file_write.writerow(val)
     
 # filepath = '/content/drive/MyDrive/DigitalCV_CourseRecommendation/'
 filepath = './'
