047cfb55be8838d09809ee4cbe5b3fb64ce96d04
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-23 17:05:42 +0700

    FIX test SQL proxy

--- test_SQL_proxy.py
+++ test_SQL_proxy.py
@@ -6,18 +6,15 @@ user = 'digitalcv-dev'
 passwd = 'tXS0CZSI+x3FLz4SG'
 database = 'digitalcv-dev'
 
-try:
-    # Connect to the database
-    connection = pymysql.connect(host='hc0vm00007.apac.bosch.com',
-                             user='digitalcv-dev',
-                             password='tXS0CZSI+x3FLz4SG',
-                             db='digitalcv-dev')
-    
-    cursor=connection.cursor()
-    cursor.execute("SELECT id, name, url FROM courses")  
-    row = cursor.fetchall()  
-    df = pandas.DataFrame(row, columns=['id', 'name', 'url'])
-    print(df)
-    print('Type of courses ', type(df))
-except :
-    print("error")
+# Connect to the database
+connection = pymysql.connect(host='hc0vm00007.apac.bosch.com',
+                          user='digitalcv-dev',
+                          password='tXS0CZSI+x3FLz4SG',
+                          db='digitalcv-dev')
+
+cursor=connection.cursor()
+cursor.execute("SELECT id, name, url FROM courses")  
+row = cursor.fetchall()  
+df = pandas.DataFrame(row, columns=['id', 'name', 'url'])
+print(df)
+print('Type of courses ', type(df))
