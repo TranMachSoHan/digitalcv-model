da419ae5eabb4df0c0f8f32a1ca7b8375dac1559
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <fixed-term.Han.TranMachSo@vn.bosch.com>
2022-08-30 15:23:14 +0700

    FIX connection sql

--- connectionSQL.py
+++ connectionSQL.py
@@ -27,95 +27,95 @@ user = 'digitalcv-dev'
 passwd = 'tXS0CZSI+x3FLz4SG'
 database = 'digitalcv-dev'
 
-
-# DEFINE THE ENGINE (CONNECTION OBJECT)
-engine = db.create_engine(f'mysql+pymysql://{user}:{passwd}@{host}/{database}')
-
-################## CREATE THE TABLE MODEL TO USE IT FOR QUERYING ##################
-class Courses(Base):
-
-    __tablename__ = 'courses'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50),
-                            primary_key=True)
-    url  = db.Column(db.String(300),
-                            primary_key=True)
-
-class Reviews(Base): 
-    __tablename__ = 'reviews'
-    id = db.Column(db.Integer, primary_key=True)
-    course_id = db.Column(db.BIGINT)
-    employee_id = db.Column(db.BIGINT)
-
-class Employees(Base): 
-    __tablename__ = 'employees'
-    id = db.Column(db.BIGINT, primary_key=True)
-    first_name = db.Column(db.String(255))
-    last_name = db.Column(db.String(255))
-
-################ START OF READ SQL FILE ################ 
-
-################ QUERY TO FETCH ALL RECORDS FOR COURSES DATABASE ################ 
-print("Getting courses from database ....")
-courses = pandas.read_sql_query(
-    sql = db.select([Courses.id,
-                        Courses.name,
-                        Courses.url,]), con=engine
-)
-courses = courses.rename(columns={'id': 'course_id'})
-
-print("Succesfully retrieved courses database. Here is the information: ")
-print('- Length of the courses in the database: ', len(courses))
-print('- Number of duplicated unique name are: ',courses.name.duplicated().sum())
-print(courses)
-print('Type of courses ', type(courses))
-print('\n')
-
-################ QUERY TO FETCH ALL RECORDS FOR REVIEWS DATABASE ##################
-print("Getting reviews from database ....")
-
-reviews = pandas.read_sql_query(
-    sql = db.select([Reviews.course_id,
-                        Reviews.employee_id,]), con=engine
-)
-
-
-print("Succesfully retrieved reviews database. Here is the information: ")
-print('- Length of the reviews in the database: ', len(reviews))
-print('Type of reviews ', type(reviews))
-print(reviews)
-print('\n')
-
-################ QUERY TO FETCH ALL RECORDS FOR EMPLOYEES DATABASE ################ 
-print("Getting employees from database ....")
-
-employees = pandas.read_sql_query(
-    sql = db.select([Employees.id,
-                        Employees.first_name,
-                        Employees.last_name,]), con=engine
-)
-# Let the reviewers name to be first_name and last_name 
-employees['reviewers'] = employees['last_name'] + ' ' + employees['first_name']
-employees = employees.rename(columns={'id': 'employee_id'})
-
-print("Succesfully retrieved employees database. Here is the information: ")
-print('- Length of the employees in the database: ', len(employees))
-print('Type of employees ', type(employees))
-print(employees)
-print('\n')
-######################## END OF READ SQL FILE ##############################
-
-
-
-######################## START OF PROCESSED SQL DATA #######################
-course_reviews = reviews.merge(employees,on = 'employee_id',how = 'inner')
-course_reviews.drop(['first_name','last_name','employee_id'], axis=1, inplace=True)
-print(course_reviews)
-######################## END OF PROCESSED SQL DATA #######################
-
-
 try:
+    # DEFINE THE ENGINE (CONNECTION OBJECT)
+    engine = db.create_engine(f'mysql+pymysql://{user}:{passwd}@{host}/{database}')
+
+    ################## CREATE THE TABLE MODEL TO USE IT FOR QUERYING ##################
+    class Courses(Base):
+
+        __tablename__ = 'courses'
+        
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(50),
+                                primary_key=True)
+        url  = db.Column(db.String(300),
+                                primary_key=True)
+
+    class Reviews(Base): 
+        __tablename__ = 'reviews'
+        id = db.Column(db.Integer, primary_key=True)
+        course_id = db.Column(db.BIGINT)
+        employee_id = db.Column(db.BIGINT)
+
+    class Employees(Base): 
+        __tablename__ = 'employees'
+        id = db.Column(db.BIGINT, primary_key=True)
+        first_name = db.Column(db.String(255))
+        last_name = db.Column(db.String(255))
+
+    ################ START OF READ SQL FILE ################ 
+
+    ################ QUERY TO FETCH ALL RECORDS FOR COURSES DATABASE ################ 
+    print("Getting courses from database ....")
+    courses = pandas.read_sql_query(
+        sql = db.select([Courses.id,
+                            Courses.name,
+                            Courses.url,]), con=engine
+    )
+    courses = courses.rename(columns={'id': 'course_id'})
+
+    print("Succesfully retrieved courses database. Here is the information: ")
+    print('- Length of the courses in the database: ', len(courses))
+    print('- Number of duplicated unique name are: ',courses.name.duplicated().sum())
+    print(courses)
+    print('Type of courses ', type(courses))
+    print('\n')
+
+    ################ QUERY TO FETCH ALL RECORDS FOR REVIEWS DATABASE ##################
+    print("Getting reviews from database ....")
+
+    reviews = pandas.read_sql_query(
+        sql = db.select([Reviews.course_id,
+                            Reviews.employee_id,]), con=engine
+    )
+
+
+    print("Succesfully retrieved reviews database. Here is the information: ")
+    print('- Length of the reviews in the database: ', len(reviews))
+    print('Type of reviews ', type(reviews))
+    print(reviews)
+    print('\n')
+
+    ################ QUERY TO FETCH ALL RECORDS FOR EMPLOYEES DATABASE ################ 
+    print("Getting employees from database ....")
+
+    employees = pandas.read_sql_query(
+        sql = db.select([Employees.id,
+                            Employees.first_name,
+                            Employees.last_name,]), con=engine
+    )
+    # Let the reviewers name to be first_name and last_name 
+    employees['reviewers'] = employees['last_name'] + ' ' + employees['first_name']
+    employees = employees.rename(columns={'id': 'employee_id'})
+
+    print("Succesfully retrieved employees database. Here is the information: ")
+    print('- Length of the employees in the database: ', len(employees))
+    print('Type of employees ', type(employees))
+    print(employees)
+    print('\n')
+    ######################## END OF READ SQL FILE ##############################
+
+
+
+    ######################## START OF PROCESSED SQL DATA #######################
+    course_reviews = reviews.merge(employees,on = 'employee_id',how = 'inner')
+    course_reviews.drop(['first_name','last_name','employee_id'], axis=1, inplace=True)
+    print(course_reviews)
+    ######################## END OF PROCESSED SQL DATA #######################
+
+
+
     ######################## READ COURSERA REVIEWS FILE ######################
     filepath = './'
     zf = ZipFile('./final.zip')
