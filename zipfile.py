133d5059f102977070eadad207206e6d60b2e2ef
FIXED-TERM Tran Mach So Han (SX/BSV21-EA) <rht9hc@bosch.com>
2022-09-07 18:33:13 +0700

    zipfile

---
+++ zipfile.py
@@ -0,0 +1,14 @@
+# importing required modules
+from json import load
+from zipfile import ZipFile
+
+def load_data():
+  print("Initializing recommender model")
+
+  filepath = './'
+  zf = ZipFile('./pickle_folder/contentBaseDf.zip')
+  # if you want to see all files inside zip folder
+  print(zf.namelist() )
+
+
+load_data()
