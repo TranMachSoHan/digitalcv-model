2a5d94db34935eb42671c75574e40629181bb20d
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-09-07 17:59:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # CORS policy 
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
+# app.config['CORS_HEADERS'] = 'Access-Control-Allow-Origin'
 
 # initialize Recommender Model class 
 r = moduleRecommendation.RecommenderModel()
