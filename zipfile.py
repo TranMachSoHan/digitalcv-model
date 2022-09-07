2fe6315c42e2c6a3191790fd623b5f750015cf0b
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2022-09-07 18:48:59 +0700

    Update zipfile.py

--- zipfile.py
+++ zipfile.py
@@ -1,12 +1,12 @@
 # importing required modules
 from json import load
-from zipfile import *
+import zipfile
 
 def load_data():
   print("Initializing recommender model")
 
   filepath = './'
-  zf = ZipFile('./pickle_folder/contentBaseDf.zip')
+  zf = zipfile.ZipFile('./pickle_folder/contentBaseDf.zip')
   # if you want to see all files inside zip folder
   print(zf.namelist() )
 
